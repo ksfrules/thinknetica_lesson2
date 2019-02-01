498c46c8c2f4242cab2979cfac15efea43a401e8
ksfrules <korabel7777@gmail.com>
2019-02-01 13:11:03 +0200

    one hash delete and change structure of another hash

--- summ_of_buyings.rb
+++ summ_of_buyings.rb
@@ -1,5 +1,5 @@
 hash_of_buings = {}
-hash_of_summs = {}
+
 loop do 
   puts "please, enter product name"
   product = gets.chomp
@@ -10,20 +10,23 @@ loop do
   puts "please, enter price for one piece"
   one_piece_price = gets.chomp.to_f
 
+  internal_hash[:price] = one_piece_price
+
   puts "please, enter total quantity of product"
   product_quantity = gets.chomp.to_f
 
-  internal_hash[one_piece_price] = product_quantity
-
+  internal_hash[:amount] = product_quantity
+  
   hash_of_buings[product] = internal_hash
 end
 
+sum = 0
+
 hash_of_buings.each do |key, hash|
-  hash_of_summs[key] = hash.flatten[0] * hash.flatten[1]
+  subtotal = hash[:price] * hash[:amount]
+  puts "{#{key} => #{hash}}" 
+  puts "Subtotal = #{subtotal}"
+  sum += subtotal
 end 
 
-puts "Here below hash with all given data"
-puts hash_of_buings
-puts "Here below hash with total prices for the product"
-puts hash_of_summs
-puts "Total summ to pay is #{hash_of_summs.values.sum}"
+puts "Total summ to pay is #{sum}"
